f15a74be85cf7230a11ac675f0677d1a1004d059
Tw3nny <boxdedarkfighter@gmail.com>
2025-11-09 23:57:01 +0100

    rm condition

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matisgutierreztw3nny <matisgutierreztw3    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 01:40:59 by matisgutier       #+#    #+#             */
-/*   Updated: 2025/11/06 22:39:25 by matisgutier      ###   ########.fr       */
+/*   Updated: 2025/11/09 23:51:45 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*tmp;
 
-	if (size != 0 && count > SIZE_MAX / size)
-		return (NULL);
 	tmp = malloc(count * size);
 	if (!tmp)
 		return (NULL);
